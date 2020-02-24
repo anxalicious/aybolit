43c5a57ef36ed315d8790434fd250d916567fd34
Anna-Maria Erik <anna-maria@meie.biz>
2020-02-24 12:55:30 +0200

    refactor(cxl-ui):cxl-switch px to switch-size-var

--- packages/storybook/cxl-switch.stories.js
+++ packages/storybook/cxl-switch.stories.js
@@ -4,11 +4,11 @@ import '@conversionxl/cxl-ui/src/components/cxl-switch.js';
 
 export default {
   decorators: [withKnobs],
-  title: 'CXL Lumo Styles|Elements'
+  title: 'CXL UI'
 };
 
 export const CxlSwitch = () => {
-  const label = text('Label', 'Dark mode');
+  const label = text('Label', 'Switch me');
 
   return html`
     <cxl-switch checked>${label}</cxl-switch>
@@ -16,5 +16,5 @@ export const CxlSwitch = () => {
 };
 
 CxlSwitch.story = {
-  name: '<cxl-switch>'
+  name: 'cxl-switch'
 };
